44e8839b07e3f875917e23983c8c067d45d0a782
Thai Phan <thaiphan90@yahoo.com>
2019-06-17 14:24:29 -0500

    created js variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,43 @@
+//Variables
+var characters = {};
+var defenders = {};
+var characterPicked = false;
+var defenderPicked = false;
+var defeated = 0;
+var gameOver = false;
+
+//Characters
+
+var obiWanKenobi = {
+    name: "Obi-Wan Kenobi",
+    health: 120,
+    attack: 12,
+    baseAttack: 12,
+    counterAttack: 12,
+};
+
+var lukeSkywalker = {
+    name: "Luke Skywalker",
+    health: 100,
+    attak: 10,
+    baseAttack: 10,
+    counterAttack: 10,
+};
+
+var darthSidious = {
+    name: "Darth Sidiious",
+    health: 150,
+    attack: 15,
+    baseAttack: 15,
+    counterAttack: 15,
+};
+
+var darthMaul = {
+    name: "Darth Maul",
+    health: 180,
+    attack: 20,
+    baseAttack: 20,
+    counterAttack: 20,
+};
+
+//Functions
